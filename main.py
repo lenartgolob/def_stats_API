9e4aa65b7f2abeeb772c07e6e5f088eda5da5519
Lenart GOlob <lenart.golob17@gmail.com>
2023-04-09 16:22:16 +1000

    added new endpoints

--- main.py
+++ main.py
@@ -1,41 +1,129 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 import json
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playercareerstats
 
 app = Flask(__name__)
-
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
+CORS(app)
 
 data = json.load(open('db.json'))
-mydb = mysql.connector.connect(
-    host="localhost",
-    user=data["user"],
-    password=data["password"],
-    port=data["port"],
-    database=data["database"]
-)
-
-mycursor = mydb.cursor()
 
 @app.get("/players")
 def get_players():
-    mycursor.execute("SELECT DISTINCT Player FROM player")
-    playerNames = mycursor.fetchall()
-    return jsonify(playerNames)
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=data["user"],
+            password=data["password"],
+            port=data["port"],
+            database=data["database"],
+        )
+
+        mycursor = mydb.cursor(dictionary=True)
+        mycursor.execute("SELECT DISTINCT Player, NbaPlayerId FROM player")
+        player_names = mycursor.fetchall()
+        mydb.close()
+        mycursor.close()
+        return jsonify(player_names)
+    except NameError:
+        return NameError, 400
 
 @app.get("/player")
 def get_player():
     try:
-        player_name = request.args.get('name')
-        print("lala")
-        print(player_name)
-        mycursor.execute(f"SELECT * FROM player WHERE Player = %s", (player_name,))
-        playerNames = mycursor.fetchall()
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=data["user"],
+            password=data["password"],
+            port=data["port"],
+            database=data["database"],
+        )
+
+        mycursor = mydb.cursor(dictionary=True)
+        nba_player_id = request.args.get('id')
+        mycursor.execute(f"SELECT SeasonYear as id, player, team, gp, min, pts, reb, ast, tov, stl, blk, ROUND(rdef, 2) as rdef, ROUND(pdef, 2) as pdef, ROUND(def, 2) as def, team, position FROM player WHERE NbaPlayerId = %s ORDER BY id", (nba_player_id,))
+        player = mycursor.fetchall()
+        mydb.close()
+        mycursor.close()
+        return jsonify(player)
     except NameError:
         return NameError, 400
-    return jsonify(playerNames)
+
+@app.get("/top/players")
+def get_top_players():
+    current_year = "22/23"
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=data["user"],
+            password=data["password"],
+            port=data["port"],
+            database=data["database"],
+        )
+
+        mycursor = mydb.cursor(dictionary=True)
+        mycursor.execute(f"SELECT id, player, team, position, gp, min, pts, reb, ast, tov, stl, blk, ROUND(rdef, 2) as rdef, ROUND(pdef, 2) as pdef, ROUND(def, 2) as def FROM player WHERE SeasonYear = %s ORDER BY DEF DESC LIMIT 20", (current_year,))
+        player = mycursor.fetchall()
+        mydb.close()
+        mycursor.close()
+        return jsonify(player)
+    except NameError:
+        return NameError, 400
+
+@app.get("/season")
+def get_season():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=data["user"],
+            password=data["password"],
+            port=data["port"],
+            database=data["database"],
+        )
+        mycursor = mydb.cursor(dictionary=True)
+
+        year = request.args.get('year')
+
+        mycursor.execute(f"SELECT id, player, team, position, gp, min, pts, reb, ast, tov, stl, blk, ROUND(rdef, 2) as rdef, ROUND(pdef, 2) as pdef, ROUND(def, 2) as def FROM player WHERE SeasonYear = %s ORDER BY DEF DESC", (year,))
+        player = mycursor.fetchall()
+        mydb.close()
+        mycursor.close()
+        return jsonify(player)
+    except NameError:
+        return NameError, 400
+
+@app.get("/status")
+def get_player_status():
+    try:
+        player_id = request.args.get('id')
+        player = [player for player in players.get_players() if player['id'] == int(player_id)][0]
+        print(player)
+        return jsonify(active=player['is_active'])
+    except NameError:
+        return NameError, 400
+
+@app.get("/top/player")
+def get_top_player():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=data["user"],
+            password=data["password"],
+            port=data["port"],
+            database=data["database"],
+        )
+        current_year = request.args.get('year').replace("-", "/")
+        mycursor = mydb.cursor(dictionary=True)
+        mycursor.execute(f"SELECT player, ROUND(rdef, 2) as rdef, ROUND(pdef, 2) as pdef, ROUND(def, 2) as def FROM player WHERE SeasonYear = %s ORDER BY DEF DESC LIMIT 1", (current_year,))
+        player_obj = mycursor.fetchall()[0]
+        mydb.close()
+        mycursor.close()
+        player_name = player_obj["player"]
+        player = [player for player in players.get_players() if player['full_name'] == player_name][0]
+        player_obj["image"] = "https://cdn.nba.com/headshots/nba/latest/1040x760/" + str(player['id']) + ".png?imwidth=1040&imheight=760"
+        return jsonify(player_obj)
+    except NameError:
+        return NameError, 400
